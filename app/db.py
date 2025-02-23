e9de2d0ec6c9207a02874df5359d28f5dc0ca7d7
vkirs229037 <vova.kirsanov.03@mail.ru>
2025-02-23 18:57:42 +0300

    Забыл точки с запятыми в запросах SQL

--- app/db.py
+++ app/db.py
@@ -15,7 +15,7 @@ def init():
                         author text,
                         year text,
                         desc text,
-                    )""")
+                    );""")
     conn.commit()
     conn.close()
 
@@ -25,7 +25,7 @@ def search(title=None, author=None, year=None, desc=None):
             "title " + ("= %s" if title is not None else "") +\
             "AND author " + ("= %s" if author is not None else "") +\
             "AND year " + ("= %s" if year is not None else "") +\
-            "AND desc " + ("= %s" if desc is not None else "")
+            "AND desc " + ("= %s" if desc is not None else "") + ";"
     args = filter(lambda p: p is not None, [title, author, year, desc])
     cur = conn.cursor()
     cur.execute(query, args)
